d85d99ff6edad8dabd2687dc7cc0b3bfb78e9d16
Sirine <sirinebenjemaa12@gmail.com>
2022-12-30 18:34:38 +0100

    first commit

---
+++ app/src/main/java/com/example/pi/hellopage.java
@@ -0,0 +1,14 @@
+package com.example.pi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hellopage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hellopage);
+    }
+}

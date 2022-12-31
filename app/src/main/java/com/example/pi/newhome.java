f25f574f79ea8a9d6659cb92e024157122a54727
Sirine <sirinebenjemaa12@gmail.com>
2023-01-01 00:59:29 +0100

    first commit

--- app/src/main/java/com/example/pi/newhome.java
+++ app/src/main/java/com/example/pi/newhome.java
@@ -1,13 +1,26 @@
 package com.example.pi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class newhome extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_newhome);
+
+
+
+
+
+
     }
 }
